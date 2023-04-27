a8d6ab751b0ec838aaa0896cf121b6c904a81bf1
BobsProgrammingAcademy <bobsprogrammingacademy@gmail.com>
2023-04-27 16:50:40 +0200

    Added unit tests

--- mail/tests.py
+++ mail/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import User, Email
 
-# Create your tests here.
+
+class ModelTesting(TestCase):
+  
+  def setUp(self):
+    self.user = User.objects.create(email='john@test.com', username='John', password='pass123')
+
+  def test_user_model(self):
+    data = self.user
+    self.assertTrue(isinstance(data, User))
+    self.assertEqual(str(data.username), 'John')
