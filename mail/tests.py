73d362829b1093026f083c51e95798c0fada8bea
BobsProgrammingAcademy <bobsprogrammingacademy@gmail.com>
2023-04-29 10:31:52 +0200

    Added unit tests

--- mail/tests.py
+++ mail/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
-from .models import User
+from django.utils import timezone
+from .models import User, Email
 
 
 class ModelTesting(TestCase):
@@ -10,3 +11,23 @@ class ModelTesting(TestCase):
         data = self.user
         self.assertTrue(isinstance(data, User))
         self.assertEqual(str(data.username), 'John')
+
+
+class EmailModelUnitTestCase(TestCase):
+    def setUp(self):
+        self.user1 = User.objects.create(email='bob@test.com', username='Bob', password='pass123')
+        self.user2 = User.objects.create(email='john@test.com', username='John', password='pass123')
+        self.email = Email.objects.create(
+            user = self.user1,
+            sender = self.user1,
+            subject = 'New email', 
+            body = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 
+            timestamp = timezone.now(),
+            read = False,
+            archived = False 
+        )
+        self.email.recipients.add(self.user2)
+
+    def test_email_model(self):
+        data = self.email
+        self.assertTrue(isinstance(data, Email))
