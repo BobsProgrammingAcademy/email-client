04476aa29b430e537fd7ec6f1b2d8475f7dd5672
Bob's Programming Academy <97769721+BobsProgrammingAcademy@users.noreply.github.com>
2024-10-25 05:46:27 +0200

    Code refactoring

--- config/settings.py
+++ config/settings.py
@@ -1,8 +1,6 @@
 import os
 
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
